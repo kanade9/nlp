12c83ac25bf18fdcfec3ab2eb50325fae2f3c855
kzmaro <maronkazu94@gmail.com>
2020-02-14 15:59:36 +0900

    ベクトル生成確認

--- test3.py
+++ test3.py
@@ -2,16 +2,14 @@ from gensim.models import KeyedVectors
 import numpy as np
 
 model = KeyedVectors.load_word2vec_format('./entity_vector.model.bin', binary=True)
-#
-# print(model['カレー'].shape)
-# print(np.array((model['こんにちは'])))
-# results = model.most_similar(u'[]')
-# for result in results:
-#     print(result)
-m = ['カレー', 'おにぎり']
 a = np.zeros((1, 200))
-
-for i in m:
-    a = np.vstack((a, np.array(model[i])))
-
-print(a)
+with open('./set_word_list.txt')as file:
+    for vo in file:
+        # print(vo)
+        try:
+            vo_model = model[vo.rstrip('\n')]
+        except:
+            continue
+        a = np.vstack((a, np.array(vo_model)))
+        # print(vo.rstrip('\n'))
+np.save('word_vector.npy', a)
